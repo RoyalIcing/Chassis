ced4e8d0332e01ebe87e1ca379644a9f956f35f5
Patrick Smith <pgwsmith@gmail.com>
2015-07-14 19:07:22 +1000

    Initial Commit

---
+++ Chassis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chassis
+//
+//  Created by Patrick Smith on 14/07/2015.
+//  Copyright (c) 2015 Burnt Caramel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Chassis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chassis
+//
+//  Created by Patrick Smith on 14/07/2015.
+//  Copyright (c) 2015 Burnt Caramel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

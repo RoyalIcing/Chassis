88c8a2de721b741a0b8682c7180c6674cf139657
Patrick Smith <pgwsmith@gmail.com>
2016-01-31 18:33:48 +1100

    Add events that component controllers can listen to

--- Chassis/ControllerTypes.swift
+++ Chassis/ControllerTypes.swift
@@ -9,15 +9,37 @@
 import Foundation
 
 
-typealias Unsubscriber = () -> Void
+typealias Unsubscriber = () -> ()
 
 typealias ElementAlterationPayload = (componentUUID: NSUUID, alteration: ElementAlteration)
 typealias ComponentMainGroupChangePayload = (mainGroup: FreeformGraphicGroup, changedComponentUUIDs: Set<NSUUID>)
 
+enum ComponentControllerEvent {
+	case Initialize([ComponentControllerEvent])
+	//case ActiveFreeformGroupChanged(group: FreeformGraphicGroup, changedComponentUUIDs: Set<NSUUID>)
+	case ActiveToolChanged(CanvasToolIdentifier)
+	case ShapeStyleForCreatingChanged(ElementReference<ShapeStyleDefinition>)
+}
+
+/*struct ComponentControllerActiveState {
+	var shapeStyleForCreating: ElementReference<ShapeStyleDefinition>
+}*/
+
+enum ComponentControllerAlterations {
+	case AlterElement(elementUUID: NSUUID, alteration: ElementAlteration)
+}
+
+protocol ComponentControllerQuerying {
+	func catalogWithUUID(UUID: NSUUID) -> Catalog?
+}
+
 
 protocol ComponentControllerType: class {
-	var mainGroupAlterationSender: (ElementAlterationPayload -> Void)? { get set }
-	var activeFreeformGroupAlterationSender: ((alteration: ElementAlteration) -> Void)? { get set }
+	//var componentControllerAlterationSender: (ComponentControllerAlterations -> ())? { get set }
+	var mainGroupAlterationSender: (ElementAlterationPayload -> ())? { get set }
+	var activeFreeformGroupAlterationSender: ((alteration: ElementAlteration) -> ())? { get set }
+	var componentControllerQuerier: ComponentControllerQuerying? { get set }
 	
-	func createMainGroupReceiver(unsubscriber: Unsubscriber) -> (ComponentMainGroupChangePayload -> Void)
+	func createMainGroupReceiver(unsubscriber: Unsubscriber) -> (ComponentMainGroupChangePayload -> ())
+	func createComponentControllerEventReceiver(unsubscriber: Unsubscriber) -> (ComponentControllerEvent -> ())
 }

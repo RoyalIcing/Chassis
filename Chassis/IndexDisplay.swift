b01bbfc887e256a1d8275f8ca70d7df69a7d4184
Patrick Smith <pgwsmith@gmail.com>
2016-03-29 15:43:26 +1100

    Add IndexDisplay enum

---
+++ Chassis/IndexDisplay.swift
@@ -0,0 +1,14 @@
+//
+//  IndexDisplay.swift
+//  Chassis
+//
+//  Created by Patrick Smith on 27/03/2016.
+//  Copyright © 2016 Burnt Caramel. All rights reserved.
+//
+
+
+public enum IndexDisplay {
+	case cardinal // 1, 2, 3
+	case ordinal // 1st, 2nd, 3rd
+	case romanNumerals(lowercase: Bool) // I, II, III
+}

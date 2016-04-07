373dae1bec5c0ee2f91d06ffd0f8940c8e34509a
Patrick Smith <pgwsmith@gmail.com>
2016-04-08 00:08:03 +1000

    Make SpanDivision JSON compatible

--- SpanDivision.swift
+++ SpanDivision.swift
@@ -7,10 +7,63 @@
 //
 
 
-public enum SpanDivision {
+public enum SpanDivision : ElementType {
 	case fraction(fraction: Dimension)
 	case distance(distance: Dimension, times: Int)
-	case evenDivisions(divisionCount: Int)
+	case equalDivisions(divisionCount: Int)
+	
+	public enum Kind : String, KindType {
+		case fraction = "fraction"
+		case distance = "distance"
+		case equalDivisions = "equalDivisions"
+	}
+	
+	public var kind: Kind {
+		switch self {
+		case .fraction: return .fraction
+		case .distance: return .distance
+		case .equalDivisions: return .equalDivisions
+		}
+	}
+}
+
+extension SpanDivision : JSONObjectRepresentable {
+	public init(source: JSONObjectDecoder) throws {
+		let type: Kind = try source.decode("type")
+		switch type {
+		case .fraction:
+			self = try .fraction(
+				fraction: source.decode("fraction")
+			)
+		case .distance:
+			self = try .distance(
+				distance: source.decode("distance"),
+				times: source.decode("times")
+			)
+		case .equalDivisions:
+			self = try .equalDivisions(
+				divisionCount: source.decode("divisionCount")
+			)
+		}
+	}
+	
+	public func toJSON() -> JSON {
+		switch self {
+		case let .fraction(fraction):
+			return .ObjectValue([
+				"fraction": fraction.toJSON()
+			])
+		case let .distance(distance, times):
+			return .ObjectValue([
+				"distance": distance.toJSON(),
+				"times": times.toJSON()
+			])
+		case let .equalDivisions(divisionCount):
+			return .ObjectValue([
+				"divisionCount": divisionCount.toJSON()
+			])
+		}
+	}
 }
 
 extension SpanDivision {
@@ -26,7 +79,7 @@ extension SpanDivision {
 			return 3
 		case let .distance(_, times):
 			return times + 1
-		case let .evenDivisions(divisionCount):
+		case let .equalDivisions(divisionCount):
 			return divisionCount + 1
 		}
 	}
@@ -46,7 +99,7 @@ extension SpanDivision {
 			}
 		case let .distance(distance, _):
 			return distance * Dimension(n)
-		case let evenDivisions(divisionCount):
+		case let equalDivisions(divisionCount):
 			return Dimension(n) * (1.0 / Dimension(divisionCount))
 		}
 	}
